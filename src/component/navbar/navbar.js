860b6a60eb5d903fbc5cc29e536136c9f0d66dc7
Rebeca <mustelierrebeca99@gmail.com>
2025-03-05 22:31:00 -0600

    updating navbar

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -2,9 +2,6 @@
  import Container from 'react-bootstrap/Container';
  import Nav from 'react-bootstrap/Nav';
  import Navbar from 'react-bootstrap/Navbar';
- import NavDropdown from 'react-bootstrap/NavDropdown';
- import Capture1 from "../../images/logo.png";
-import Capture from "../../pages/layout/imagenes/Capture.PNG";
 import styles from "./navbar.module.css";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 import { CgMail } from "react-icons/cg";
@@ -16,24 +13,20 @@ import { CgMail } from "react-icons/cg";
       <Container>
         <Navbar.Brand href="/" >
                 <div>
-                    <img className={styles.image} src={Capture1}></img>
+                  logo
+                    {/* <img className={styles.image} src={Capture1}></img> */}
                 </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className={styles.btn__displacement} href="/">Home</Nav.Link>
-            <Nav.Link className={styles.btn__displacement} href="/faqs">Faqs</Nav.Link>
-            <Nav.Link className={styles.btn__displacement} href="/gallery">Coments</Nav.Link>
-            <Nav.Link className={styles.btn__displacement} href="/contact">Contact Us</Nav.Link>
+            <Nav.Link className={styles.btn__displacement} href="/mi-carrito">Mi carrito</Nav.Link>
             {/* <Nav.Link className={styles.btn__displacement} href="/bookOnline">Book Online</Nav.Link> */}
     
           </Nav>
           <Nav className={styles.btn__social__media__container}>
-            <Nav.Link className={styles.btn__social__media} href="https://www.facebook.com/profile.php?id=100082191186102"><FaFacebookF /></Nav.Link>
-            <Nav.Link className={styles.btn__social__media} href="https://www.instagram.com/hellentrinchet/"><FaInstagram /></Nav.Link>
-            <Nav.Link className={styles.btn__social__media} href="/"><FaTwitter /></Nav.Link>
-            <Nav.Link className={styles.btn__social__media} href="/emails"><CgMail /></Nav.Link>
+            <Nav.Link className={styles.btn__social__media} href="/"><FaFacebookF className={styles.icon}/></Nav.Link>
+            <Nav.Link className={styles.btn__social__media} href="/"><FaInstagram className={styles.icon}/></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/layout/layout.js
+++ src/pages/layout/layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from "./layout.module.css";
+import NavBar from '../../component/navbar/navbar';
 
 export default function Layout({ children }) {
     return (
         <div className={styles.main__container}>
             <div   className={styles.layout}>
-                {/* <NavBar /> */}
+                <NavBar />
 
                 <div className={styles.body}>
                     {children}
